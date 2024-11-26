c62c6f6c3276443f341904196f2347a648690349
Gido Manders <gido.manders@42.nl>
2024-11-26 17:37:54 +0100

    fix: use correct error message when throwing exception in shipment request

--- src/DPDShipment.php
+++ src/DPDShipment.php
@@ -232,11 +232,11 @@ class DPDShipment{
         }
         catch (SoapFault $e)
         {
-            Log::emergency('DPD: '.$e->faultstring);
+            Log::emergency('DPD: '.$e->faultstring, [$e->detail]);
             if ($this->client) {
                 Log::debug('DPD: SOAP-Request Shipment: ' . $this->client->__getLastRequest());
             }
-            throw new DPDException('SOAP Fehler ' . $e->faultstring);
+            throw new DPDException($e->detail != null ? $e->detail->faults->message : $e->faultstring);
         }
 
     }
